41848294641bcefcb4e76b0e8ae7655e162bc2f2
Mohammad Kaif <95179984+kaifatuni@users.noreply.github.com>
2022-01-20 14:57:52 +0530

    Blog 0.32

---
+++ pages/contact.js
@@ -0,0 +1,37 @@
+import Layout from "../components/Layout";
+import NavOne from "../components/NavOne";
+import Footer from "../components/Footer";
+import Blog from "../components/Blog";
+
+export default function Custom404() {
+    return (
+      <div>
+        <Layout/>
+        <NavOne/>
+        <section className="kaif-one banner-one" id="banner">
+          <span className="banner-one__shape-1"></span>
+          <span className="banner-one__shape-2"></span>
+          <span className="banner-one__shape-3"></span>
+          <span className="banner-one__shape-4"></span>
+          <div className="container">
+            <div className="kaif-one__carousel">
+                <div className="kaif-one__single">
+                  <div className="kaif-one__inner">
+                    <p>
+                      Contact!!!!
+                    </p>
+                    <h3>404</h3>
+                    {/* <span></span>
+                    <img
+                      src="/assets/images/resources/testi-1-1.png"
+                      alt="Awesome Image"
+                    /> */}
+                  </div>
+                </div>
+            </div>
+          </div>
+        </section>
+        <Footer/>
+      </div>
+    )
+  }
